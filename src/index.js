33a9ec6e8f46b8a9f0ff5b755ea82863c9b3bf3e
Wahida <wahidagull1395@gmail.com>
2024-11-27 18:14:47 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 function displayTemperature(response) {
+  let description = document.querySelector("#description");
   let temperature = document.querySelector("#current-temperature");
+  let humidity = document.querySelector("#humidity");
+  let windspeed = document.querySelector("#wind-speed");
+  let currentDateELement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
   temperature.innerHTML = Math.round(response.data.temperature.current);
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeed.innerHTML = `${response.data.wind.speed}k/h`;
+  currentDateELement.innerHTML = formatDate(date);
 }
 
 function citys(event) {
@@ -18,6 +27,29 @@ function citys(event) {
 let searchform = document.querySelector("#search-form");
 searchform.addEventListener("submit", citys);
 
-let now = new Date();
-let datetime = document.querySelector("#date-time");
-datetime.innerHTML = now;
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
