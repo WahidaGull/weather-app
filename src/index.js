3c7ff652eddbd335c09532758724c5bf8c370d62
Wahida <wahidagull1395@gmail.com>
2024-11-27 16:05:26 +0100

    html and css changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 function displayTemperature(response) {
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(response.data.temperature.current) + "°C";
+  let temperature = document.querySelector("#current-temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
 }
 
 function citys(event) {
   event.preventDefault();
 
-  let h1 = document.querySelector("#city");
-  let enterCity = document.querySelector("#enterCity");
+  let h1 = document.querySelector("#current-city");
+  let searchinput = document.querySelector("#search-input");
   let apiKey = "42205924203081247ba4o2b1310et7f4";
-  let city = enterCity.value;
+  let city = searchinput.value;
+  h1.innerHTML = city;
   let Urlapi = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(Urlapi).then(displayTemperature);
-  h1.innerHTML = city;
 }
-let searching = document.querySelector("#searching");
-searching.addEventListener("submit", citys);
+
+let searchform = document.querySelector("#search-form");
+searchform.addEventListener("submit", citys);
 
 let now = new Date();
 let datetime = document.querySelector("#date-time");
